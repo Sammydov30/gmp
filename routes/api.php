11f6f833bf3da84ad5ca5d7d463397cd928fd958
Samuel Chuks <samydov@gmail.com>
2023-12-10 12:59:34 +0100

    gmp

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,7 @@ Route::prefix('v1')->group(function () {
     Route::middleware(['auth:sanctum', 'type.customer'])->group(function () {
         Route::post('/customer/logout', [CustomerAuthController::class, 'logout']);
         Route::get('/customer', [CustomerController::class, 'index']);
-        Route::get('/customer/editprofile', [CustomerController::class, 'editprofile']);
+        Route::get('/customer/fetchprofile', [CustomerController::class, 'editprofile']);
         Route::post('/customer/updateprofile', [CustomerController::class, 'updateprofile']);
         Route::get('/customer/listaddress', [CustomerController::class, 'listaddress']);
         Route::post('/customer/addaddress', [CustomerController::class, 'addaddress']);
