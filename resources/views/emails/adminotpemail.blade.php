4f174218d742fb42d3415acaed8ae961520a7f9c
Samuel Chuks <samydov@gmail.com>
2024-01-06 14:42:30 +0100

    gmp

--- resources/views/emails/adminotpemail.blade.php
+++ resources/views/emails/adminotpemail.blade.php
@@ -1,31 +1,80 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns:v="urn:schemas-microsoft-com:vml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
-        <meta name="viewport" content="width=600,initial-scale = 2.3,user-scalable=no">
+<!DOCTYPE html>
+<html lang="en">
 
-        <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,700" rel="stylesheet">
-    <title> OTP Verification </title>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="{{ asset('logo.png') }}" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,700;1,200&display=swap" rel="stylesheet">
+  <title>OTP Verification</title>
 </head>
-<body style="padding: 10px;  ">
-    <center><img src="{{ asset('logo.png') }}" style="width: 300px; height: auto;" alt=""></center>
-<p>&nbsp; &nbsp;&nbsp;</p>
+<style>
+  body {
+    background-color: #f8f8f8;
+  }
 
-<center><h2><span style="font-family: Montserrat"><strong>{{$details['subject']}}&nbsp;</strong></span></h2></center>
-<p style="line-height: 24px;margin-bottom:15px;">
-    Your call-a-doc Admin OTP is: <br><br>
-</p>
+  b {
+    color: #ff7b00;
+    font-weight: 600;
+  }
 
-<p>
+  .policy {
+    color: #95989b;
+  }
 
-    <span></span> <span><em>{{$details['otp']}}</em></span>
-</p>
+  .btn-block {
+    background-color: #fb7b00;
+    border-radius: 20px;
+    color: #ffffff;
+    width: 50%;
+  }
+  .link{
+    text-decoration: none;
+    color:#ff7b00;
+  }
+  p{
+    font-family: 'Poppins', sans-serif;
+  }
+</style>
 
+<body>
 
-<p>&nbsp;</p>
+  <div class="row justify-content-center align-items-center" style="height: 100vh;">
+    <div class="col-md-6">
+      <div style="background-color: #ffffff; border: none;" class="card"></div>
+      <div class="container mt-5">
+        <img src="{{ asset('assets/img/gavice.png') }}" alt="Gavice" class="img-fluid" style="height:80px;"><br>
+        <div style="background-color: #ffffff; border: 1px solid #ffffff;" class="card">
+          <div class="card-body">
+            <p>Hi there,</p>
+            <p>{{$details['subject']}}</p><br>
+            <p>Your GMP Admin OTP is:</p>
+            <p>{{$details['otp']}}</p>
+          </div>
+        </div><br>
+        <p style="font-weight: 700;">Get inspired on social</p>
+        <div class="social-icons mt-3">
+          <a href=""><img src="{{ asset('assets/img/twitter.png') }}" alt="twitter" class="img-fluid"></a>
+          <a href=""><img src="{{ asset('assets/img/facebook.png') }}" alt="facebook" class="img-fluid"></a>
+          <a href=""><img src="{{ asset('assets/img/linkedin.png') }}" alt="linkedin" class="img-fluid"></a>
+        </div>
+        <p class="policy">Need help? Try our <b>HelpCenter</b> or reach out to <b><a class="link" href="#">support@gavicemarketplace.com</a></b>. if you would rather
+          not receive this kind of email, you can <b>unsubscribe</b> or <b>manage your email preferences</b></p>
+        <p class="policy">By continuing to use GMP, you are accepting our
 
+          <a href=""><b>Terms of Service</b></a> and
+          <a href=""><>Privacy policy</b></a>.
+        </p>
+        <p class="policy">GFHV+9GX, Ojike St, Bende Rd, &, Umuahia 440236, Abia</p>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
--- resources/views/emails/registeremail.blade.php
+++ resources/views/emails/registeremail.blade.php
@@ -1,35 +1,85 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns:v="urn:schemas-microsoft-com:vml">
+<!DOCTYPE html>
+<html lang="en">
 
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
-        <meta name="viewport" content="width=600,initial-scale = 2.3,user-scalable=no">
-
-        <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,700" rel="stylesheet">
-    <title> Registration Successful </title>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="{{ asset('logo.png') }}" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,700;1,200&display=swap" rel="stylesheet">
+  <title> Registration Successful </title>
 </head>
-<body style="padding: 10px;  ">
-    <center><img src="{{ asset('gmp-logo.png') }}" alt=""></center>
-<p>&nbsp; &nbsp;&nbsp;</p>
+<style>
+  body {
+    background-color: #f8f8f8;
+  }
 
-<h2><span style="font-family:Comic Sans MS, cursive"><strong>Welcome {{$details['name']}} ,&nbsp;</strong></span></h2>
-<h3>Its official, you’re now a part of the GMP community!!</h3>
+  b {
+    color: #ff7b00;
+    font-weight: 600;
+  }
 
-<p>
-<span style="text-align: center;">
-    Get ready to enjoy the best delivery services at your fingertips.
-    Think of us as your best companions when it comes to reliable, swift and efficient deliveries.
+  .policy {
+    color: #95989b;
+  }
 
-    Can’t wait? Log on to your account and request a delivery now.
+  .btn-block {
+    background-color: #fb7b00;
+    border-radius: 20px;
+    color: #ffffff;
+    width: 50%;
+  }
+  .link{
+    text-decoration: none;
+    color:#ff7b00;
+  }
+  p{
+    font-family: 'Poppins', sans-serif;
+  }
+</style>
 
-    Feel free to contact us if you have any problem or suggestion. Email us via info@gavicemarketplace.com or call +2348137519848
-</span>
-</p>
+<body>
 
-<p>&nbsp;</p>
+  <div class="row justify-content-center align-items-center" style="height: 100vh;">
+    <div class="col-md-6">
+      <div style="background-color: #ffffff; border: none;" class="card"></div>
+      <div class="container mt-5">
+        <img src="{{ asset('assets/img/gavice.png') }}" alt="Gavice" class="img-fluid" style="height:80px;"><br>
+        <div style="background-color: #ffffff; border: 1px solid #ffffff;" class="card">
+          <div class="card-body">
+            <p>Hi there,</p>
+            <h2>Welcome {{$details['name']}} ,&nbsp;</strong></span></h2>
+            <p>Its official, you’re now a part of the GMP community!!</p><br>
+            <p>Get ready to enjoy the best delivery services at your fingertips.
+                Think of us as your best companions when it comes to reliable, swift and efficient deliveries.
+                Can not wait? Log on to your account and request a delivery now.
+                Feel free to contact us if you have any problem or suggestion.
+                Email us via support@gavicemarketplace.com or call +2348137519848</p><br>
+          </div>
+        </div><br>
+        <p style="font-weight: 700;">Get inspired on social</p>
+        <div class="social-icons mt-3">
+          <a href=""><img src="{{ asset('assets/img/twitter.png') }}" alt="twitter" class="img-fluid"></a>
+          <a href=""><img src="{{ asset('assets/img/facebook.png') }}" alt="facebook" class="img-fluid"></a>
+          <a href=""><img src="{{ asset('assets/img/linkedin.png') }}" alt="linkedin" class="img-fluid"></a>
+        </div>
+        <p class="policy">Need help? Try our <b>HelpCenter</b> or reach out to <b><a class="link" href="#">support@gavicemarketplace.com</a></b>.
+        if you would rather
+          not receive this kind of email, you can <b>unsubscribe</b> or <b>manage your email preferences</b></p>
+        <p class="policy">By continuing to use GMP, you are accepting our
 
+          <a href=""><b>Terms of Service</b></a> and
+          <a href=""><>Privacy policy</b></a>.
+        </p>
+        <p class="policy">GFHV+9GX, Ojike St, Bende Rd, &, Umuahia 440236, Abia</p>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
