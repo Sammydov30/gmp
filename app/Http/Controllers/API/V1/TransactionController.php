c55cc8b29ef4e2f639e1edacf3fb4cb3de39ea6b
Samuel Chuks <samydov@gmail.com>
2024-02-21 13:29:42 +0100

    GMP

--- app/Http/Controllers/API/V1/TransactionController.php
+++ app/Http/Controllers/API/V1/TransactionController.php
@@ -74,8 +74,8 @@ class TransactionController extends Controller
 
         $transactions=$result->orderBY($sortBy, $sortOrder)->paginate($perPage);
 
-        //return response()->json($transactions, 200);
-        return TransactionResource::collection($transactions);
+        return response()->json($transactions, 200);
+        //return TransactionResource::collection($transactions);
     }
 
     public function show($id)
