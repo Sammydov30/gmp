e0fbac26cbaca10b97bca59ad18483eeba97773a
Samuel Chuks <samydov@gmail.com>
2024-06-08 19:42:13 +0100

    Monday

---
+++ app/Http/Resources/API/V1/Customer/ProductResource2.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\API\V1\Customer;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource2 extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'      =>  $this->id,
+            'storeid' => $this->storeid,
+            'marketid' => $this->marketid,
+            'gmpid' => $this->gmpid ,
+            'name' => $this->name,
+            'amount' => $this->amount,
+            'category' => $this->category,
+            'description' => $this->description,
+            'status' => $this->status,
+            'market'   => $this->market,
+            'store' => $this->store,
+            'images' => ProductImagesResource::collection($this->getMedia('images')),
+        ];
+    }
+}
--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Http\Resources\API\V1\Customer\ProductResource;
+use App\Http\Resources\API\V1\Customer\ProductResource2;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -22,21 +23,21 @@ class Cart extends Model
     public function toArray()
     {
         $array = parent::toArray();
-        $array['item2'] = $this->GetProductDetails($this->product);
+        $array['item'] = $this->GetProductDetails($this->product);
 
         return $array;
     }
 
-    public function item()
-    {
-        return $this->hasOne(Product::class, 'id', 'product');
-    }
+    // public function item()
+    // {
+    //     return $this->hasOne(Product::class, 'id', 'product');
+    // }
     private function GetProductDetails($item){
         $product=Product::with('market', 'store', 'productimages')->find($item);
         if (!$product) {
             return null;
         }
-        $product = new ProductResource($product);
+        $product = new ProductResource2($product);
         //Convert to json then to array (To get Pure array)
         //$item=json_decode(json_encode($productt), true);
         //print_r($item); exit();
