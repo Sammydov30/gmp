f667b8463dcb044f0280ea020853240c39248232
Samuel Chuks <samydov@gmail.com>
2024-06-08 19:35:00 +0100

    Monday

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Http\Resources\API\V1\Customer\ProductResource;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -18,9 +19,28 @@ class Cart extends Model
         'confirmed',
     ];
 
-    public function item()
+    public function toArray()
     {
-        return $this->hasOne(Product::class, 'id', 'product');
+        $array = parent::toArray();
+        $array['item'] = $this->GetProductDetails($this->product);
+
+        return $array;
+    }
+
+    // public function item()
+    // {
+    //     return $this->hasOne(Product::class, 'id', 'product');
+    // }
+    private function GetProductDetails($item){
+        $product=Product::with('market', 'store', 'productimages')->find($item);
+        if (!$product) {
+            return null;
+        }
+        $product = new ProductResource($product);
+        //Convert to json then to array (To get Pure array)
+        //$item=json_decode(json_encode($productt), true);
+        //print_r($item); exit();
+        return $product;
     }
 
 
