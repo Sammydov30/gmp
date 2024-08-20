6344e56d97ded3a30b7ae1da7587b72412e2af72
Samuel Chuks <samydov@gmail.com>
2024-08-20 18:06:50 +0100

    Reporting and Feedback

--- app/Traits/ActiveSubscriptionTrait.php
+++ app/Traits/ActiveSubscriptionTrait.php
@@ -4,7 +4,7 @@ namespace App\Traits;
 
 use App\Models\Subscription;
 
-trait GMPCustomerBalanceTrait {
+trait ActiveSubscriptionTrait {
 
     public function activeSubscription() {
         $user=auth()->user();
